0dc373ffe45d1a67c68fd4ae50d42c9164c237c6
none23 <none23>
2017-02-07 01:41:21 +0300

    updated

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -20,41 +20,40 @@ theme.taglist_shape = function(cr, width, height)
 end
 --}}}]]
 -- Colors {{{
-theme.fg                  = "#999999"
-theme.bg                  = "#000000"
-theme.primary             = "#de5e1e"
-theme.primary2            = "#ff6000"
-theme.secondary           = "#1e9dde"
-theme.danger              = "#de1e1e"
-theme.black               = "#000000"
-theme.tranasparent        = "#00000000"
-theme.midgray_0           = "#141414"
-theme.midgray_1           = "#232323"
-theme.midgray_2           = "#060606"
+theme.fg               = "#999999"
+theme.bg               = "#000000"
+theme.primary          = "#de5e1e"
+theme.primary2         = "#ff6000"
+theme.secondary        = "#1e9dde"
+theme.danger           = "#de1e1e"
+theme.black            = "#000000"
+theme.tranasparent     = "#00000000"
+theme.midgray_0        = "#141414"
+theme.midgray_1        = "#232323"
+theme.midgray_2        = "#060606"
 
-theme.popup_fg            = "#12121288"
-theme.popup_bg            = theme.primary
-theme.popup_danger_bg     = "#de1e1e88"
-theme.popup_danger_fg     = theme.bg
+theme.popup_fg         = "#12121288"
+theme.popup_bg         = theme.primary
+theme.popup_danger_bg  = "#de1e1e88"
+theme.popup_danger_fg  = theme.bg
 
-theme.fg_normal           = theme.fg
-theme.fg_focus            = theme.primary2
-theme.fg_urgent           = theme.black
-theme.menu_fg_normal      = theme.fg
-theme.menu_fg_focus       = theme.primary
-theme.fg_minimize         = theme.fg
-theme.taglist_fg_focus    = theme.primary
+theme.fg_normal        = theme.fg
+theme.fg_focus         = theme.primary2
+theme.fg_urgent        = theme.black
+theme.menu_fg_normal   = theme.fg
+theme.menu_fg_focus    = theme.primary
+theme.fg_minimize      = theme.fg
+theme.taglist_fg_focus = theme.primary
 
-theme.bg_normal           = theme.bg
-theme.bg_focus            = theme.bg
-theme.bg_urgent           = theme.primary
-theme.menu_bg_normal      = theme.bg
-theme.menu_bg_focus       = theme.bg
-theme.bg_minimize         = theme.bg
-theme.bg_systray          = theme.bg
-theme.prompt_fg = theme.bg
-theme.prompt_bg = theme.primary
-
+theme.bg_normal        = theme.bg
+theme.bg_focus         = theme.bg
+theme.bg_urgent        = theme.primary
+theme.menu_bg_normal   = theme.bg
+theme.menu_bg_focus    = theme.bg
+theme.bg_minimize      = theme.bg
+theme.bg_systray       = theme.bg
+theme.prompt_fg_cursor = theme.bg
+theme.prompt_bg_cursor = theme.primary
 
 -- }}}
 -- Menu {{{
